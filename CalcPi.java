b9b1ac54a3208f883539bfe4353ff85f4257e3c4
Roi Refaeli <roi.refaeli@post.runi.ac.il>
2025-11-08 08:43:09 -0800

    Completed assignment

--- CalcPi.java
+++ CalcPi.java
@@ -2,7 +2,7 @@
 public class CalcPi {
 	public static void main(String [] args) { 
 		double Pie = Math.PI;
-		System.out.println("Pi according to Java: " + Pie);
+		System.out.println("pi according to Java: " + Pie);
 		int amount = Integer.parseInt(args[0]);
 		double orinalPie = 1.0;
 		double k = 3.0;
@@ -13,7 +13,7 @@ public class CalcPi {
 				orinalPie -= (1 / k );}
 		}
 		orinalPie = orinalPie * 4;
-		System.out.println("Pi, approximated:     " + orinalPie);
+		System.out.println("pi, approximated:     " + orinalPie);
 	
 	}
 }
