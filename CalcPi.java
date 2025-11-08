8599e193289ea2b8a63d3ef0f448dc2f5f4e4e8a
Roi Refaeli <roi.refaeli@post.runi.ac.il>
2025-11-08 08:36:21 -0800

    Completed assignment

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,19 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+		double Pie = Math.PI;
+		System.out.println("Pi according to Java: " + Pie);
+		int amount = Integer.parseInt(args[0]);
+		double orinalPie = 1.0;
+		double k = 3.0;
+		for (int i = 1; i < amount; i++, k += 2) {
+			if (i % 2 == 0) {
+				orinalPie += (1 / k );
+			} else {
+				orinalPie -= (1 / k );}
+		}
+		orinalPie = orinalPie * 4;
+		System.out.println("Pi, approximated:     " + orinalPie);
+	
 	}
 }
